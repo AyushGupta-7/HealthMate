d5a772c47c3c601271afffdc545a1cc4ae13133e
Ayush Gupta <ayufer9@gmail.com>
2026-04-27 09:27:05 +0530

    first commit

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,34 +1,3 @@
-// const errorHandler = (err, req, res, next) => {
-//   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-//   let message = err.message;
-
-//   // Mongoose duplicate key
-//   if (err.code === 11000) {
-//     statusCode = 400;
-//     message = 'Duplicate field value entered';
-//   }
-
-//   // Mongoose validation error
-//   if (err.name === 'ValidationError') {
-//     statusCode = 400;
-//     message = Object.values(err.errors).map(val => val.message).join(', ');
-//   }
-
-//   // Mongoose Cast error (invalid ObjectId)
-//   if (err.name === 'CastError') {
-//     statusCode = 404;
-//     message = 'Resource not found';
-//   }
-
-//   res.status(statusCode).json({
-//     success: false,
-//     message,
-//     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-//   });
-// };
-
-// export default errorHandler;
-
 export const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
